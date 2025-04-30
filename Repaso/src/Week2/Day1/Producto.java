87a636e1eba61113889433eee1ae985103041a34
Samuel <samuelayllonsevilla86@gmail.com>
2025-04-30 12:01:41 +0200

    Add files via upload

---
+++ Repaso/src/Week2/Day1/Producto.java
@@ -0,0 +1,37 @@
+package Week2.Day1;
+
+public class Producto {
+    private String name;
+    private double price;
+    private int stock;
+
+    public Producto(String name, int stock, double price) {
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
